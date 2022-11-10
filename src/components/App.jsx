a528b4fc4827d24b5f5996181fd1523e2e5d7971
Валерія Рибонька <valeria@Valerias-MacBook-Pro.local>
2022-11-10 11:37:32 +0200

    bug fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,60 +7,56 @@ import { Container } from './App.styled';
 
 import { useEffect } from 'react';
 
-const STORAGE_KEY ='contacts'
-let contactList =[
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]
+const STORAGE_KEY = 'contacts';
+let contactList = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 export const App = () => {
-  const [contacts, setContacts] = useState (contactList);
+  const [contacts, setContacts] = useState(contactList);
   const [filter, setFilter] = useState('');
-  const firstRender = useRef(true)
-  
+  const firstRender = useRef(true);
 
-  const hendlerAddContact = newContact => {
-    
-    if (contacts.filter(({ name }) => name === newContact.name).length !== 0) {
-      alert( `${newContact.name} is already in contacts!`);
-      return;
-    }
-   
-    setContacts(state=> [newContact, ...state]);
-  };
-
-  const onDeleteContact = id => {
-    setContacts(state => (
-      state.filter(contact => contact.id !== id)
-    ));
-  };
-  const handleFilterChange = filter => {
-    setFilter(filter);
-  };
-
-  const filterContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter) 
-    );
-  };
   useEffect(() => {
     const contacts = localStorage.getItem(STORAGE_KEY);
-    const parsedContacts = JSON.parse(contacts)
+    const parsedContacts = JSON.parse(contacts);
     if (parsedContacts) {
-      setContacts( parsedContacts);
+      setContacts(parsedContacts);
     }
-  },[])
+  }, []);
+
   useEffect(() => {
     if (firstRender.current) {
       firstRender.current = false;
       return;
     }
     localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
-  },[contacts])
+  }, [contacts]);
 
+  const hendlerAddContact = newContact => {
+    if (contacts.filter(({ name }) => name === newContact.name).length !== 0) {
+      alert(`${newContact.name} is already in contacts!`);
+      return;
+    }
+    setContacts(state => [newContact, ...state]);
+  };
+
+  const onDeleteContact = id => {
+    setContacts(state => state.filter(contact => contact.id !== id));
+  };
+
+  const handleFilterChange = filter => {
+    setFilter(filter);
+  };
+
+  const filterContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
 
   return (
     <Container>
@@ -69,18 +65,18 @@ export const App = () => {
       </Section>
       <Section title="Contacts">
         {contacts.length > 0 ? (
-          <> <Filter
-          filter={filter}
-          onFilterChange={handleFilterChange}
-        />
-
-        <ContactList
-          contacts={filterContacts()}
-          onDeleteContact={onDeleteContact}
+          <>
+            {' '}
+            <Filter filter={filter} onFilterChange={handleFilterChange} />
+            <ContactList
+              contacts={filterContacts()}
+              onDeleteContact={onDeleteContact}
             />
-          </>) : (<p>You phonebook is empty! Add your first contact</p>)} 
-       
+          </>
+        ) : (
+          <p>You phonebook is empty! Add your first contact</p>
+        )}
       </Section>
     </Container>
   );
-}
+};
